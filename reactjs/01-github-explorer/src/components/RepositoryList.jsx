b74de903de10432ca267665a67d1d5e6de1f93f2
Alexandre Gomes <xandmaga@outlook.com>
2021-03-16 09:26:11 -0300

    Finalizando o fetch dos repositorios do github

--- reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,5 +1,8 @@
 import { RepositoryItem } from "./RepositoryItem"
 import "../styles/repositories.scss"
+import { useEffect, useState } from "react"
+
+// https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
     name: "unform",
@@ -8,13 +11,23 @@ const repository = {
 }
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [])
+
     return (
         <section className="repository-list">
             <h1>Repository List</h1>
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem />
-                <RepositoryItem />
+                {
+                    repositories.map(repository => {
+                        return <RepositoryItem key={repository.name} repository={repository} />
+                    })
+                }
             </ul>
         </section>
     )
